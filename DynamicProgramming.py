4ce3d1a64aed886569cf43b68da92043bcbd8ce7
SpaceCoder123 <mohanraam2000@gmail.com>
2025-11-16 13:16:54 +0530

    Removed unwated lines

--- DynamicProgramming.py
+++ DynamicProgramming.py
@@ -134,25 +134,19 @@ class DynamicProgramming:
     def canJump(self, nums):
         n = len(nums)
         memo = set()
-        
         def canJumpHelder(index):
             if index == n - 1:
                 return True
-            
             if index >= n:
                 return False
-            
             if index in memo:
                 return False
-            
             max_jump = index + nums[index]
-            
             for next_index in range(index + 1, max_jump + 1):
                 if canJumpHelder(next_index):
                     return True
             memo.add(index)
             return False
-        
         return canJumpHelder(0)
 
     #optimal
@@ -162,9 +156,7 @@ class DynamicProgramming:
         for i in range(len(nums)):
             if i > max_reach:
                 return False
-            
             max_reach = max(max_reach, i + nums[i])
-            
             if max_reach >= len(nums) - 1:
                 return True
         return True
